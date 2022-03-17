f7e57ab5ab4be81c9e46dc8533265fd43183fc44
Claudia <claudiatcruz2002@gmail.com>
2022-03-17 20:21:00 +0100

    Program acabado

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,9 +10,33 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
-
-            // Keep the console window open in debug mode.
+            Pila ini = new Pila();
+            Pila aux = new Pila();
+            Pila fin = new Pila();
+            int n = 3;
+            Console.WriteLine("El numero de discos es"+ n);
+            for (int i = n; i>0;i--)
+            {
+                Disco disco = new Disco();
+                disco.Valor = i;
+                ini.push(disco);
+            }
+            Hanoi hanoi = new Hanoi();
+            Console.WriteLine("A través del modo iterativo");
+            double resultado = hanoi.iterativo(n, ini, fin, aux);
+            Console.WriteLine("El numero de movimientos fue de: "+ resultado);
+            //calculo los movimientos mínimos
+            double movimientos_min = Math.Pow(2, n)-1;
+            if (resultado == movimientos_min)
+            {
+                Console.WriteLine("Corresponde con el número mínimo de movimientos");
+            }
+            else
+            {
+                Console.WriteLine("No corresponde con el número mínimo de movimietnos");
+            }
             Console.WriteLine("Press any key to exit.");
+           
             Console.ReadKey();
         }
     }
